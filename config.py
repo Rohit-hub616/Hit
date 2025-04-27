facded60814a769e1236a366a6ae4480b4514e3f
Rohit-hub616 <ruhilrohit9@gmail.com>
2025-04-27 14:14:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 api_id = "25967358"
 api_hash = "10a5a31171dfdc323efdbcf84a8fb016"
-bot_token = "7898501804:AAHDf5GWhPt3glMSxrKMZkCxTD_aMOXnCrE"
+bot_token = "7059710796:AAHustbji45EixwjP0GEmiN2qn4tsAngQag"
 auth_users = [7733608738]
